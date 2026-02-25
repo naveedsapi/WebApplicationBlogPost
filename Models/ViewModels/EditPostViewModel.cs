e3ed055829ae2428f13a55dd0c10320bcbfdb5b6
Sapi <naveedkhansapi@gmail.com>
2026-02-25 11:16:48 +0100

    this property is used to display the data in the edit form and also to update the data in the database

---
+++ Models/ViewModels/EditPostViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApplicationBlogPost.Models.ViewModels
+{
+    public class EditPostViewModel
+    {
+        /// <summary>
+        /// this property is used to display the data in the edit form and also to update the data in the database
+        /// </summary>
+        public Post Post { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IFormFile FeatureImage { get; set; }
+    }
+}
