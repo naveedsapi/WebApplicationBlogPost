a3eb9ab40073152dccc2279ce9f95080a471ceae
Sapi <naveedkhansapi@gmail.com>
2026-02-28 01:20:51 +0100

    Validate Never Applyed for Blog Post

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplicationBlogPost.Models
 {
@@ -12,7 +13,7 @@ namespace WebApplicationBlogPost.Models
         public string Name { get; set; }
     
         public string? Description { get; set; }
-
+        [ValidateNever]
         public ICollection<Post> Posts { get; set; }
     }
 }
