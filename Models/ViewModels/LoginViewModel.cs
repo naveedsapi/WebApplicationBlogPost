3cb480c11726269530960786497ec586638992b1
Sapi <naveedkhansapi@gmail.com>
2026-03-02 11:42:15 +0100

    RegisterViewModel Added Successfuly

---
+++ Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationBlogPost.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email Is Requird Field")]
+        [EmailAddress(ErrorMessage = "Email Must be In Correct Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password Is Requird Field")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        
+    }
+}
---
+++ Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationBlogPost.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email Is Requird Field")]
+        [EmailAddress(ErrorMessage = "Email Must be In Correct Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password Is Requird Field")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password",ErrorMessage = "Password Must match The ConfirmPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
