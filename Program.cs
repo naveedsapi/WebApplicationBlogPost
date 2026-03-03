21240e331e154851dc7675faee179e604e02064f
Sapi <naveedkhansapi@gmail.com>
2026-03-03 11:23:48 +0100

    To add Admin Role to Database using Scope in Program cs

--- Program.cs
+++ Program.cs
@@ -29,11 +29,36 @@ builder.Services.AddIdentity<IdentityUser,IdentityRole>(options =>
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
+    options.LoginPath = "/Auth/Login";
+    options.AccessDeniedPath = "/Auth/AccessDenied";
     options.ExpireTimeSpan = TimeSpan.FromDays(7);
     options.SlidingExpiration = true;
 });
 
 var app = builder.Build();
+
+// this code for Roll Base Access of the Application
+using (var scope = app.Services.CreateScope())
+{
+    var _userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    var _roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var adminEmail = "admin@gmail.com";
+    var adminPassword = "Admin";
+
+    var existingAdminRole = await _roleManager.FindByNameAsync("Admin");
+    if (existingAdminRole == null)
+    {
+        await _roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+    var existingAdminUser = await _userManager.FindByEmailAsync(adminEmail);
+    if (existingAdminUser == null)
+    {
+        var adminUser = new IdentityUser { UserName = adminEmail, Email = adminEmail };
+        await _userManager.CreateAsync(adminUser, adminPassword);
+        await _userManager.AddToRoleAsync(adminUser, "Admin");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
